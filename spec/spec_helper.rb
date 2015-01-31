309906a3c4b42ba27729dabc6de0bc2dd46835f7
pollseed <cb.so.devope+0001@gmail.com>
2015-01-31 18:58:10 +0900

    add template

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,14 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# Capibaraが動かないから入れた
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
 RSpec.configure do |config|
+  config.include Capybara::DSL
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -82,4 +89,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  #config.include Capybara::DSL
 end
