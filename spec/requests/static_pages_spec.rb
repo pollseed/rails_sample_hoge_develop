445d62284a66c75c4de0a9e1787cbb84b62dcf85
pollseed <cb.so.devope+0001@gmail.com>
2015-02-01 15:59:16 +0900

    add layout,helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", :type => :request do
-#  describe "GET /static_pages" do
-#    it "works! (now write some real specs)" do
-#      get static_pages_index_path
-#      expect(response).to have_http_status(200)
-#    end
-#  end
 
   describe "Home page" do
     it "should have the content 'Sample App'" do
@@ -16,7 +10,12 @@ RSpec.describe "StaticPages", :type => :request do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('|Home')
     end
   end
 
